65cef5058838be1fb9d2e0de52131ed8d774de5a
BlablaTara <tara0561@stud.kea.dk>
2026-04-14 20:39:58 +0200

    added new test-database

--- utils/supabaseConnection.js
+++ utils/supabaseConnection.js
@@ -1,9 +1,10 @@
 import 'react-native-url-polyfill/auto'
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kpjwfgukvwxfwkxqjaji.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwandmZ3Vrdnd4ZndreHFqYWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NDUwMDQsImV4cCI6MjA2MzMyMTAwNH0.qXHsYOuB5M7h_etDFVzbSxf4DmtSob2Fc-eUJKkVvT4";
+const supabaseUrl = "https://mwadswxpdbuwzdevaiak.supabase.co";
+const supabaseKey = "sb_publishable_MvK8qT9riBInt4UUd2v0Qw_5y6gLnN5";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
